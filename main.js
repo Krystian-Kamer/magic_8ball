23ef48972f196484df7a57bcd071f78caa63e89c
Krystian <texen24@gmail.com>
2023-05-20 23:15:59 +0200

    enden project

--- main.js
+++ main.js
@@ -1,12 +1,7 @@
-const blackBill = document.querySelector('img')
+const ball = document.querySelector('img')
 const input = document.querySelector('input')
 const answer = document.querySelector('.answer')
 const error = document.querySelector('.error')
-
-const span = document.createElement('span')
-answer.append(span)
-span.textContent = 'Odpowiedź:'
-
 
 const arrAnswer = [
 	'Kości zostały rzucone',
@@ -24,6 +19,31 @@ const arrAnswer = [
 	'To nie jest to',
 	'Zejdź na ziemię',
 	'No raczej nie inaczej',
-	'Nie masz czasem uczucia, że to co robisz nie ma sensu?',
+	'Nie masz czasem uczucia, że to co robisz nie ma sensu ?',
 ]
 
+const shakeBall = () => {
+	ball.classList.add('shake-animation')
+	setTimeout(checkInput, 1000)
+}
+
+const checkInput = () => {
+	ball.classList.remove('shake-animation')
+	answer.innerHTML = ''
+
+	if (input.value.includes('?')) {
+		generateAnswer()
+		error.textContent = ''
+	} else if (!input.value.includes('?') && input.value !== '') {
+		error.textContent = 'Pytanie musi zawierać "?"'
+	} else {
+		error.textContent = 'Musisz zadać jakieś pytanie'
+	}
+}
+
+const generateAnswer = () => {
+	const newArray = arrAnswer[Math.floor(Math.random() * arrAnswer.length)]
+	answer.innerHTML = `<span>Odpowiedź:</span> ${newArray}`
+}
+
+ball.addEventListener('click', shakeBall)
